d35253dd8f55e8b8c2919dab88aa3730a2c3df65
pranav <prnvthir@gmail.com>
2021-01-14 23:23:46 -0500

    added fetchCoordinatgesByIP to retrive user coordinates

--- index.js
+++ index.js
@@ -19,3 +19,19 @@ fetchMyIP((error, myIP) => {
 });
 
 fetchMyIP()
+
+// The code below is temporary and can be commented out.
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('142.122.84.196', (error, coordinates) => {
+  
+  if (error) {
+  
+    console.log("It didn't work!" , error);
+  
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+
+});
--- iss.js
+++ iss.js
@@ -39,4 +39,27 @@ const fetchMyIP = function (callback) {
 
 // fetchMyIP()
 
-module.exports = fetchMyIP;
+
+const fetchCoordsByIP = function(ip, callback) {
+  
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+
+
+
+module.exports = {fetchMyIP,fetchCoordsByIP}
